576508fca51594207f535bb7d71d2f6c1060eeaa
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2023-07-26 12:54:14 +0000

    sortFu.py

---
+++ sortFu.py
@@ -0,0 +1,18 @@
+# function to return the second element of the
+# two elements passed as the parameter
+def sortSecond(val):
+	return val[1]
+
+# list1 to demonstrate the use of sorting
+# using second key
+list1 = [(1,2),(3,3),(1,1)]
+
+# sorts the array in ascending according to
+# second element
+list1.sort(key=sortSecond)
+print(list1)
+
+# sorts the array in descending according to
+# second element
+list1.sort(key=sortSecond,reverse=True)
+print(list1)
